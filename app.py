fccd8385c4108d31b4dc20ee61295a5c63b6d049
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-02 16:24:24 +0100

    Added base project files to start with

---
+++ app.py
@@ -0,0 +1,10 @@
+from lib.database_connection import DatabaseConnection
+
+
+# Connect to the database
+connection = DatabaseConnection()
+connection.connect()
+
+# Seed with some seed data
+connection.seed("seeds/book_store.sql")
+
